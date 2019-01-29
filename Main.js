af3826cb92d2ddf42ad97b6b114c3bb6cfec4faa
cppkyle <33187539+cppkyle@users.noreply.github.com>
2019-01-29 09:09:02 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,72 +1,72 @@
-this.canvas = document.getElementById('canvas');
-this.ctx = canvas.getContext('2d');
-this.inGame = false;
-this.sawTutorial = true;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var inGame = false;
+var sawTutorial = true;
+var menu, game, tutorial;
 
-this.init();
+init();
 
 function init() {
     window.addEventListener('resize', resizeCanvas, false);
     canvas.addEventListener('pointerdown', mouseClicked, false);
     canvas.addEventListener('mousedown', mouseClicked, false);
-    //canvas.addEventListener('touchstart', mouseClicked, false);
-    this.sounds = [new Audio('./click1.mp3'),new Audio('./wrong.mp3'),new Audio('./Ice.mp3'), new Audio('./Double.mp3')];
-    this.score = localStorage.getItem('score');
-    if (this.score === null) {
-       this.score = 0;
+    sounds = [new Audio('./click1.mp3'),new Audio('./wrong.mp3'),new Audio('./Ice.mp3'), new Audio('./Double.mp3')];
+    score = localStorage.getItem('score');
+    if (score === null) {
+       score = 0;
     }
-    this.menu = new Menu(this.score, 0);
-    this.resizeCanvas();
-    this.draw();
+    menu = new Menu(score, 0);
+    resizeCanvas();
+    draw();
 }
 
 function draw() {
 	if (inGame) {
-		this.game.draw();
-		if (this.game.centerDiameter <= 10) {
-			if (this.game.points > this.score) {
-				this.score = this.game.points;
-				localStorage.setItem('score', this.game.points);
+		game.draw();
+		if (game.centerDiameter <= 10) {
+			if (game.points > score) {
+				score = game.points;
+				localStorage.setItem('score', game.points);
 			}
-			this.endPlaying(this.game.points);
+			endPlaying(game.points);
 		}
 	} else {
 		menu.draw();
-		if (this.sawTutorial == false) {
-			this.tutorial.draw();
+		if (sawTutorial == false) {
+			tutorial.draw();
 		}
 	}
 	window.requestAnimationFrame(draw);
 }
 
 function resizeCanvas() {
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-    this.canvas.style.width = "100%";
-    this.canvas.style.height = "100%";
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
 }
 
 function mouseClicked(event) {
-	if (this.inGame) {	
-		this.game.mouseClicked(event);
-	} else if (this.sawTutorial == false) {
-		if (event.clientX >= this.tutorial.rectSize[0] - this.tutorial.rectSize[2]/2 && event.clientX <= this.tutorial.rectSize[0] + this.tutorial.rectSize[2] && event.clientY >= this.tutorial.rectSize[1] - this.tutorial.rectSize[3]/2 && event.clientY <= this.tutorial.rectSize[1] + this.tutorial.rectSize[3]) {
-			this.sawTutorial = true;
-			this.menu = new Menu(this.score, this.canvas, this.ctx);
+	if (inGame) {	
+		game.mouseClicked(event);
+	} else if (sawTutorial == false) {
+		if (event.clientX >= tutorial.rectSize[0] - tutorial.rectSize[2]/2 && event.clientX <= tutorial.rectSize[0] + tutorial.rectSize[2] && event.clientY >= tutorial.rectSize[1] - tutorial.rectSize[3]/2 && event.clientY <= tutorial.rectSize[1] + tutorial.rectSize[3]) {
+			sawTutorial = true;
+			menu = new Menu(score, canvas, ctx);
 		} 		
 	} else {
-		if (event.clientX >= this.menu.rectSize[0] - this.menu.rectSize[2]/2 && event.clientX <= this.menu.rectSize[0] + this.menu.rectSize[2]/2 && event.clientY >= this.menu.rectSize[1] - this.menu.rectSize[3]/2 && event.clientY <= this.menu.rectSize[1] + this.menu.rectSize[3]/2) {
-				this.startPlaying();
+		if (event.clientX >= menu.rectSize[0] - menu.rectSize[2]/2 && event.clientX <= menu.rectSize[0] + menu.rectSize[2]/2 && event.clientY >= menu.rectSize[1] - menu.rectSize[3]/2 && event.clientY <= menu.rectSize[1] + menu.rectSize[3]/2) {
+				startPlaying();
 		}	
 	}
 }
 
 function startPlaying() {
-	this.inGame = true;
-	this.game = new Game(this.sounds, this.canvas, this.ctx);
+	inGame = true;
+	game = new Game(sounds, canvas, ctx);
 }
 
 function endPlaying(lastScore) {
-	this.inGame = false;
-	this.menu = new Menu(this.score, lastScore);
+	inGame = false;
+	menu = new Menu(score, lastScore);
 }
